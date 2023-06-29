2294326211abf35b8fb16c42942cb963df296e6e
alofgren <drelofren@outlook.com>
2023-06-30 00:57:00 +0200

    Reformatted

--- setup.py
+++ setup.py
@@ -4,24 +4,25 @@ import commandalarm
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup (
-    name = "CommandAlarm",
-    version = commandalarm.__version__,
-    author = "alofgren",
-    author_email = "drelofren@outlook.com",
-    description = "A simple command line program that allows users to set an alarm with a custom command.",
-    license = "GPLv3",
-    keywords = ["alarm", "command", "command-line", "tool", "timer", "commandalarm"],
-    url = "https://github.com/alofgren/commandalarm",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    packages = setuptools.find_packages(),
-    entry_points = {
-        "console_scripts": [
-            "commandalarm = commandalarm.commandalarm:main"
-        ]
+setuptools.setup(
+    name="CommandAlarm",
+    version=commandalarm.__version__,
+    author="alofgren",
+    author_email="drelofren@outlook.com",
+    description=("A simple command line program that allows users to set"
+                 " an alarm with a custom command."),
+    license="GPLv3",
+    keywords=[
+        "alarm", "command", "command-line", "tool", "timer", "commandalarm"
+    ],
+    url="https://github.com/alofgren/commandalarm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ["commandalarm = commandalarm.commandalarm:main"]
     },
-    classifiers = [
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Intended Audience :: End Users/Desktop",
@@ -32,10 +33,13 @@ setuptools.setup (
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Utilities",
     ],
-    project_urls = {
-        "Source": "https://github.com/alofgren/commandalarm",
-        "Bug Reports": "https://github.com/alofgren/commandalarm/issues",
-        "Changelog": "https://github.com/alofgren/commandalarm/blob/main/CHANGELOG.md",
+    project_urls={
+        "Source":
+        "https://github.com/alofgren/commandalarm",
+        "Bug Reports":
+        "https://github.com/alofgren/commandalarm/issues",
+        "Changelog":
+        "https://github.com/alofgren/commandalarm/blob/main/CHANGELOG.md",
     },
-    python_requires = ">=3.7"
+    python_requires=">=3.7",
 )
