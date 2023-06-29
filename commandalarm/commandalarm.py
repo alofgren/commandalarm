e448cb0bb376d1d7c6d7b07c8d1818594da4282a
alofgren <drelofren@outlook.com>
2023-06-30 01:30:44 +0200

    Updated the docstring

--- commandalarm/commandalarm.py
+++ commandalarm/commandalarm.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-""" Set an alarm with a custom command. """
+"""CommandAlarm main module"""
 
 import argparse
 import datetime
