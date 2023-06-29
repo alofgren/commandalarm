ebf4482f44264f8c8e2435fc7ce12aa052ebe72c
alofgren <drelofren@outlook.com>
2023-06-30 01:40:50 +0200

    Updated the docstring

--- commandalarm/__init__.py
+++ commandalarm/__init__.py
@@ -13,6 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-""" Set an alarm with a custom command. """
+"""CommandAlarm"""
 
 __version__ = "0.2.8"
